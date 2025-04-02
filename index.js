82b1edb99fa902c96abf4452d16190519ae6f833
mccryy <mccryy@gmail.com>
2025-04-01 21:35:47 -0300

    Some changes

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ let xpHeroi = 4000
 
 switch (true) {
     case (xpHeroi <= 1000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Ferro`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Ferro\n`)
         break
     case (xpHeroi >= 1001 && xpHeroi <= 2000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Prata`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Prata\n`)
         break
     case (xpHeroi >= 2001 && xpHeroi <= 5000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Prata`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Prata\n`)
         break
     case (xpHeroi >= 5001 && xpHeroi <= 7000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Ouro`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Ouro\n`)
         break
     case (xpHeroi >= 7001 && xpHeroi <= 8000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Platina`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Platina\n`)
         break
     case (xpHeroi >= 8001 && xpHeroi <= 9000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Ascendente`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Ascendente\n`)
         break
     case (xpHeroi >= 9001 && xpHeroi <= 10000):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Imortal`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Imortal\n`)
         break
     case (xpHeroi >= 10001):
-        console.log(`O Herói de nome ${nomeHeroi} está no nível `+`Radiante`)
+        console.log(`\nO Herói de nome ${nomeHeroi} está no nível `+`Radiante\n`)
         break
 }
